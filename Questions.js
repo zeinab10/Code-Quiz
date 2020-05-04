37e8103cdfd5ea5dad0f41ac4e32f26030e75419
Zeinab <31262989+zeinab10@users.noreply.github.com>
2020-05-03 22:46:11 -0500

    Update Questions.js

---
+++ Questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        correctAnswer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        correctAnswer: "parentheses"
+    },
+    {
+        title: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+        choices: ['<script name = "xxx.js">', '<script src = "xxx.js">', '<script href = "xxx.js">', '<script call = "xxx.js">'],
+        correctAnswer: '<script src = "xxx.js">',
+    },
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    }
+];
