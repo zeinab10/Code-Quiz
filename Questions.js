147217896b2093b4df8fad5b63039d4c0f899864
Zeinab <zeinab@Zeinabs-MacBook-Air.local>
2020-05-19 00:01:26 -0500

    add

--- Questions.js
+++ Questions.js
@@ -1,12 +1,3 @@
-// select all elements
-const start = document.getElementById("start");
-const quiz = document.getElementById("quiz");
-const question = document.getElementById("question");
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
-const counter = document.getElementById("counter");
-const timeGauge = document.getElementById("timeGauge");
 const progress = document.getElementById("progress");
 const scoreDiv = document.getElementById("scoreContainer");
 
--- script.js
+++ script.js
@@ -72,3 +72,5 @@ const questions =[
         ]
     }
 ]
+
+//coment
